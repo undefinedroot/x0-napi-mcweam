65315b007c3902572f48fd1868dd3ee76122715b
undefinedroot <57220840+undefinedroot@users.noreply.github.com>
2020-08-14 21:42:38 +0800

    setup initial routes for bootcamp controller, some initial setup for Postman collections

--- DevCamper/api/server.js
+++ DevCamper/api/server.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const dotenv = require('dotenv');
+// const logger = require('./middleware/logger');
+const morgan = require('morgan');
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+//app.use(logger); /* use custom middleware */
+
+// Dev logging middleware, only use it on development mode
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.disable('x-powered-by');
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000; /* access with help of dotenv */
 
