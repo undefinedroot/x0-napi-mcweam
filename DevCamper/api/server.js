5a59306e96537bc9234084a8294301f71e472fac
undefinedroot <57220840+undefinedroot@users.noreply.github.com>
2020-08-15 09:08:06 +0800

    setup code for remote connection on mongodb atlas, added promise rejection error handler when cannot connect to remote database, imported test data, created Bootcamp model, completed CRUD functionality for Bootcamp (postman and code).

--- DevCamper/api/server.js
+++ DevCamper/api/server.js
@@ -2,16 +2,24 @@ const express = require('express');
 const dotenv = require('dotenv');
 // const logger = require('./middleware/logger');
 const morgan = require('morgan');
-
-// Route files
-const bootcamps = require('./routes/bootcamps');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
+// Connect to database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
 
 //app.use(logger); /* use custom middleware */
+
+// Body parser, so that we can retrieve the posted JSON data from the body property of the request object
+app.use(express.json());
 
 // Dev logging middleware, only use it on development mode
 if (process.env.NODE_ENV === 'development') {
@@ -25,4 +33,11 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000; /* access with help of dotenv */
 
-app.listen(PORT, console.log(`Server running in '${process.env.NODE_ENV}' mode on port: ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in '${process.env.NODE_ENV}' mode on port: ${PORT}`.yellow.bold));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
