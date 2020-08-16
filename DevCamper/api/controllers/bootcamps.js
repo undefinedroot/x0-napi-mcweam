73899be8df6ae7e0fdbb0e277002e5626804969e
undefinedroot <57220840+undefinedroot@users.noreply.github.com>
2020-08-16 21:54:48 +0800

    implemented 'pagination' operation on request queries.

--- DevCamper/api/controllers/bootcamps.js
+++ DevCamper/api/controllers/bootcamps.js
@@ -15,7 +15,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   const reqQuery = { ...req.query };
 
   // fields to exclude on filtering (these are the keywords that should not appear as value of fields in a query)
-  const removeFields = ['select', 'sort'];
+  const removeFields = ['select', 'sort', 'page', 'limit'];
 
   // loop  over removeFields and delete them from reqQuery
   removeFields.forEach(param => delete reqQuery[param]);
@@ -44,10 +44,41 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     query = query.sort('-createdAt');
   }
 
+  // pagination
+  const page = parseInt(req.query.page, 10) || 1; /* base 10 parse, default 'page' as 1 */
+  const limit = parseInt(req.query.limit, 10) || 25;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Bootcamp.countDocuments();
+
+  query = query.skip(startIndex).limit(limit);
+
   // execute query
   const bootcamps = await query;
 
-  res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps });
+  // Pagination result
+  const pagination = {};
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit
+    };
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit
+    }
+  }
+
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    pagination,
+    data: bootcamps
+  });
 });
 //#region
 /* old code:
